398678f44bb2891d6bd3adba0c57e5caf6b840a2
Дмитрий Цветков <tchv71@mail.ru>
2025-10-24 20:03:52 +0300

    Fix sync

--- WIZnet-PICO-C/port/ioLibrary_Driver/src/w5x00_spi.c
+++ WIZnet-PICO-C/port/ioLibrary_Driver/src/w5x00_spi.c
@@ -162,14 +162,14 @@ static void wizchip_write_burst(uint8_t *pBuf, uint16_t len)
 
 static void wizchip_critical_section_lock(void)
 {
-    //critical_section_enter_blocking(&g_wizchip_cri_sec);
-    MTX_ENTER();
+    critical_section_enter_blocking(&g_wizchip_cri_sec);
+    //MTX_ENTER();
 }
 
 static void wizchip_critical_section_unlock(void)
 {
-    //critical_section_exit(&g_wizchip_cri_sec);
-    MTX_EXIT();
+    critical_section_exit(&g_wizchip_cri_sec);
+    //MTX_EXIT();
 }
 
 void wizchip_spi_initialize(void)
